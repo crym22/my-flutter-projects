2043f02f929b2c23e6fef2ef1c5e6ba4b63dec31
lexerotk <lexerotk@gmail.com>
2024-03-01 11:11:56 +0300

    deneme

---
+++ enum/constructor.dart
@@ -0,0 +1,11 @@
+enum Departman { Yazilimci, Pazarlamaci, Yonetici }
+
+class Calisan {
+  late String ad;
+  late String soyad;
+  late Departman departman;
+  late String adres;
+  late int mass;
+  late String telefon;
+  Calisan(this.ad, this.soyad, this.departman); //CONSTRUCTOR
+}
